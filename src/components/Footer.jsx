ad5e4243304b83b4b178360eaf4d3e33891418f8
Gastón Jaureguiberry <gaston.jaureguib@gmail.com>
2026-06-26 18:02:23 -0300

    footer mejorado, creo que le gane a la card no estan preciosas pero andan en galeria

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,55 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Importamos Link
+import { Link } from 'react-router-dom';
+import './Footer.css';
 
-function Navbar() {
+function Footer() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
-      <div className="container-fluid px-4">
-        
-        {/* LOGO: Usamos Link to="/" */}
-        <Link className="navbar-brand d-flex align-items-center" to="/">
-          <img src="/logo.png" alt="Spooter Logo" height="40" className="me-2" />
-      
-        </Link>
+    <footer className="app-footer">
 
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarSupportedContent" 
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <Link to="/" className="footer-btn home">
+        🏠 Home
+      </Link>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          
-          {/* NAVEGACIÓN: Link to="..." */}
-          <ul className="navbar-nav mx-auto mb-2 mb-lg-0 gap-2">
-            <li className="nav-item">
-              <Link className="nav-link fw-semibold" to="/"><i className="bi bi-house-door me-1"></i>Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link fw-semibold" to="/album/perros"><i className="bi bi-dog me-1"></i>Perros</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link fw-semibold" to="/album/gatos"><i className="bi bi-cat me-1"></i>Gatos</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link fw-semibold" to="/album/plantas"><i className="bi bi-tree me-1"></i>Plantas</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link fw-semibold" to="/comunidad"><i className="bi bi-journal-album me-1"></i>Comunidad</Link>
-            </li>
-          </ul>
+      <Link to="/album/perros" className="footer-btn dogs">
+        🐶 Perros
+      </Link>
 
-          <div className="d-flex">
-            
-          </div>
+      <Link to="/album/gatos" className="footer-btn cats">
+        🐱 Gatos
+      </Link>
 
-        </div>
-      </div>
-    </nav>
+      <Link to="/album/plantas" className="footer-btn plants">
+        🌿 Plantas
+      </Link>
+
+      <Link to="/comunidad" className="footer-btn community">
+        🌎 Comunidad
+      </Link>
+
+    </footer>
   );
 }
 
-export default Navbar;
+export default Footer;
